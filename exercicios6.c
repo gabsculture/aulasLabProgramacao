fd64c779e58d9f5dbdcae985fac3c28df3a54404
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-09-26 15:45:29 -0300

    ta dabdi errado

--- exercicios6.c
+++ exercicios6.c
@@ -7,10 +7,10 @@ int calculaImc(int altura, int peso) {
 
 int main() {
   int altura, peso, imc;
-  printf("Por favor digite sua altura\n");
+  printf("Por favor digite sua altura em m\n");
   scanf("%d", &altura);
 
-  printf("Por favor digite seu peso\n");
+  printf("Por favor digite seu peso em kg\n");
   scanf("%d", &peso);
 
   imc = calculaImc(altura, peso);
