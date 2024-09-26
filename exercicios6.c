d8c53428d687c25758a05a4ecfc93e5c39b682d6
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-09-26 15:50:12 -0300

    agora ta certo vamoooo

--- exercicios6.c
+++ exercicios6.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int calculaImc(int altura, int peso) {
-  int imc = peso * 10000 / altura * altura;
+  int imc = peso * 10000 / (altura * altura);
   return imc;
 }
 
 int main() {
   int altura, peso, imc;
-  printf("Por favor digite sua altura em m\n");
+  
+  printf("Por favor digite sua altura em cm\n");
   scanf("%d", &altura);
 
   printf("Por favor digite seu peso em kg\n");
