54c1fe7edefa1d107c7fe6ff8506c8c3fcb56264
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-10-25 22:50:41 -0300

    Update trabalhoGabs.c

--- trabalhoGabs.c
+++ trabalhoGabs.c
@@ -1,5 +1,6 @@
 //t1 l124b gabriela vitoria da rosa soares
 
+#include <stdatomic.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,7 +9,9 @@ void imprimeCaracteres(int quantidade, char caractere) {
     for (int i = 0; i < quantidade; i++) {
         printf("%c", caractere);
     }
-    printf("\n");
+    // if (caractere != ' ') {
+    // printf("\n");
+    // }
 }
 
 //Função que retorna a figura A: aumenta um caractere por linha
@@ -21,7 +24,7 @@ void imprimeFiguraA(char caractere, int altura) {
 
 //Função que retorna a figura B: diminui um caractere por linha
 void imprimeFiguraB(char caractere, int altura) {
-    for (int linha = altura; linha = 1; linha--) {
+    for (int linha = altura; linha >= 0; linha--) {
         int numeroCaracteres = linha - 1;
         imprimeCaracteres(numeroCaracteres, caractere);
     }
@@ -39,7 +42,7 @@ void imprimeFiguraC(char caractere, int altura) {
 
 //Função que retorna a figura D: diminui um caractere e aumenta um espaço por linha
 void imprimeFiguraD(char caractere, int altura) {
-    for (int linha = altura; linha = 1; linha--) {
+    for (int linha = altura; linha >= 1; linha--) {
         int numeroCaracteres = linha - 1;
         int numeroEspacos = altura - (numeroCaracteres + 1);
         imprimeCaracteres(numeroEspacos, ' ');
@@ -47,6 +50,7 @@ void imprimeFiguraD(char caractere, int altura) {
     }
 }
 
+//Função que retorna a figura E: diminui um espaço e aumenta dois caracteres por linha
 void imprimeFiguraE(char caractere, int altura) {
     for (int linha = 0; linha < altura; linha++) {
         int numeroEspacos = altura - linha - 1;
@@ -63,7 +67,7 @@ void imprimeFiguraF(char caractere, int altura) {
         imprimeCaracteres(numeroEspacos, ' ');
         imprimeCaracteres(numeroCaracteres, caractere);
     }
-    for(int linha  = altura - 2; linha >= 0; linha--) {
+    for (int linha = altura - 2; linha >= 0; linha--) {
         int numeroEspacos = altura - linha - 1;
         int numeroCaracteres = 2 * linha + 1;
         imprimeCaracteres(numeroEspacos, ' ');
@@ -71,16 +75,100 @@ void imprimeFiguraF(char caractere, int altura) {
     }
 }
 
+//"g": parecido, mas para uma linha de n caracteres é 1 caractere + n-2 espaços + 1 caractere;
 void imprimeFiguraG(char caractere, int altura) {
+    for (int linha = 0; linha < altura; linha++) {
+        int numeroEspacos = altura - linha - 1;
+        int numeroCaracteres = 2 * linha + 1;
+
+        if (numeroCaracteres > 1) {
+            int numeroEspacosEntreCaracteres = numeroCaracteres - 1;
+            imprimeCaracteres(numeroEspacos, ' ');
+            printf("%c", caractere);
+            imprimeCaracteres(numeroEspacosEntreCaracteres, ' ');
+            printf("%c", caractere);
+            printf("\n");
+        } else {
+            imprimeCaracteres(numeroEspacos, ' ');
+            imprimeCaracteres(numeroCaracteres, caractere);
+        }
+    }
+    for (int linha = altura - 2; linha >= 0; linha--) {
+        int numeroEspacos = altura - linha - 1;
+        int numeroCaracteres = 2 * linha + 1;
+        if (numeroCaracteres > 1) {
+            int numeroEspacosEntreCaracteres = numeroCaracteres - 1;
+            imprimeCaracteres(numeroEspacos, ' ');
+            printf("%c", caractere);
+            imprimeCaracteres(numeroEspacosEntreCaracteres, ' ');
+            printf("%c", caractere);
+            printf("\n");
+        } else {
+            imprimeCaracteres(numeroEspacos, ' ');
+            imprimeCaracteres(numeroCaracteres, caractere);
+        }
+    }
 }
 
 void imprimeFiguraH(char caractere, int altura) {
+    for (int linha = 0; linha <= altura; linha++) {
+        int numeroEspacos = 2 * linha + 1;
+        int numeroCaracteres = altura * 2;
+        if (linha == 0) {
+            numeroCaracteres = altura * 2 + 1;
+            imprimeCaracteres(numeroCaracteres, caractere);
+            printf("\n");
+        } else if (numeroEspacos >= 1 && linha != altura - 1) {
+            int numeroEspacosEntreCaracteres = altura - linha - 1;
+            int numeroCaracteresPorLado = numeroCaracteres / 2;
+            imprimeCaracteres(numeroCaracteresPorLado, caractere);
+            imprimeCaracteres(numeroEspacosEntreCaracteres, ' ');
+            imprimeCaracteres(numeroCaracteresPorLado, caractere);
+            printf("\n");
+        } else if (numeroCaracteres % 2 != 0 && linha == altura - 1) {
+            int numeroCaracteresPorLado = ((numeroCaracteres - 1) / 2) - 2;
+            int numeroEspacosEntreCaracteres = altura - linha;
+            imprimeCaracteres(1, caractere);
+            imprimeCaracteres(numeroEspacosEntreCaracteres, ' ');
+            imprimeCaracteres(numeroCaracteresPorLado, caractere);
+            imprimeCaracteres(numeroEspacosEntreCaracteres, ' ');
+            imprimeCaracteres(1, caractere);
+            printf("\n");
+        } else if ((numeroCaracteres-1) % 2 != 0) {
+            int numeroCaracteresPorLado = (numeroCaracteres - 1)/ 2;
+            int numeroEspacosEntreCaracteres = altura - linha;
+            imprimeCaracteres(numeroCaracteresPorLado, caractere);
+            imprimeCaracteres(numeroEspacosEntreCaracteres, ' ');
+            imprimeCaracteres(1, caractere);
+            imprimeCaracteres(numeroEspacosEntreCaracteres, ' ');
+            imprimeCaracteres(numeroCaracteresPorLado, caractere);
+            printf("\n");
+        } else {
+            imprimeCaracteres(numeroEspacos, ' ');
+            imprimeCaracteres(numeroCaracteres, caractere);
+            printf("\n");
+        }
+    }
+}
+
+char escolheCaractere() {
+    char caractereDesenho;
+    printf("Escolha um caractere para desenhar a figura:\n");
+    scanf("%c", &caractereDesenho);
+    return caractereDesenho;
+}
+
+int escolheQuantidadeLinhas() {
+    int quantidadeLinhas;
+    printf("Escolha a quantidade de linhas para desenhar:\n");
+    scanf("%d", &quantidadeLinhas);
+    return quantidadeLinhas;
 }
 
 int main() {
     char opcaoFigura;
     char caractereDesenho;
-    int quantidadeCaracteres;
+    int quantidadeLinhas;
 
     printf("Ola, usuario! Bem-vindo ao sistema de desenho de figuras!\n");
 
@@ -90,37 +178,54 @@ int main() {
 
     switch (opcaoFigura) {
         case 'a':
-            // printf("Escolha um caractere para desenhar a figura:\n");
-            // scanf("%c", &caractereDesenho);
-            //
-            // printf("Escolha a quantidade de caracteres para desenhar:\n");
-            // scanf("%d", &quantidadeCaracteres);
-
-            imprimeFiguraA('*', 4);
-
+            //caractereDesenho = escolheCaractere();
+            //quantidadeLinhas = escolheQuantidadeLinhas();
+            imprimeFiguraA('*', 3);
             break;
 
         case 'b':
+            //caractereDesenho = escolheCaractere();
+            //quantidadeLinhas = escolheQuantidadeLinhas();
+            imprimeFiguraB('*', 3);
+            break;
 
         case 'c':
-            printf("Escolha um caractere para desenhar a figura:");
-            scanf("%c", &caractereDesenho);
-            printf("Escolha a quantidade de caracteres para desenhar:");
-            scanf("%d", &quantidadeCaracteres);
-            imprimeFiguraC(caractereDesenho, quantidadeCaracteres);
+            //caractereDesenho = escolheCaractere();
+            //quantidadeLinhas = escolheQuantidadeLinhas();
+            imprimeFiguraC('*', 3);
             break;
 
         case 'd':
+            //caractereDesenho = escolheCaractere();
+            //quantidadeLinhas = escolheQuantidadeLinhas();
+            imprimeFiguraD('*', 3);
+            break;
 
         case 'e':
+            //caractereDesenho = escolheCaractere();
+            //quantidadeLinhas = escolheQuantidadeLinhas();
+            imprimeFiguraE('*', 3);
+            break;
 
         case 'f':
+            //caractereDesenho = escolheCaractere();
+            //quantidadeLinhas = escolheQuantidadeLinhas();
+            imprimeFiguraF('*', 3);
+            break;
 
         case 'g':
+            //caractereDesenho = escolheCaractere();
+            //quantidadeLinhas = escolheQuantidadeLinhas();
+            imprimeFiguraG('*', 3);
+            break;
 
         case 'h':
+            //caractereDesenho = escolheCaractere();
+            //quantidadeLinhas = escolheQuantidadeLinhas();
+            imprimeFiguraH('*', 3);
+            break;
 
         case 'z':
-            exit(0);
+            printf("Tchau!");
     }
 }
