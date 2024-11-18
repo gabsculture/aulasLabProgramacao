0fa6790ae299a7bf69e3bc909e8bd121018a0d71
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-11-18 20:20:40 -0300

    Update t2.c

--- t2.c
+++ t2.c
@@ -1,16 +1,4 @@
-/******************************************************************************
-1. Faça uma função que recebe um tabuleiro contendo rainhas e retorna true se o tabuleiro
-contiver uma configuração n-rainhas, conforme descrito no final da aula 15. A função deve
-suportar tabuleiro de qualquer tamanho, diferente do que está descrito na aula 15.
-Para isso, recebe 2 argumentos: um inteiro com o tamanho do tabuleiro e um vetor
-de char que codifica o tabuleiro.
-2. Faça uma função para desenhar o tabuleiro. A função deve receber o tamanho do tabuleiro
-e o vetor de char que o codifica. Use as funções abaixo, que permitem posicionar o
-cursor e alterar a cor do que será escrito na tela.
-/home/gabs/t2gabs/t2.c
-*******************************************************************************/
 #include <stdio.h>
-#include <string.h>
 #include <stdbool.h>
 #include "terminal.h"
 #include <time.h>
@@ -87,6 +75,7 @@ bool processa_entrada(char tabuleiro[], int tamanho, int *linha_cursor, int *col
 
     char tecla = t_tecla();
 
+    ```c
     if (tecla == 'w' && *linha_cursor > 1) {
         (*linha_cursor)--;
     } else if (tecla == 's' && *linha_cursor < tamanho) {
@@ -95,17 +84,14 @@ bool processa_entrada(char tabuleiro[], int tamanho, int *linha_cursor, int *col
         (*coluna_cursor)--;
     } else if (tecla == 'd' && *coluna_cursor < tamanho) {
         (*coluna_cursor)++;
-    }
-
-    else if (tecla == '\n' || tecla == ' ') {
+    } else if (tecla == '\n' || tecla == ' ') {
         int index = ((*linha_cursor - 1) * tamanho) + (*coluna_cursor - 1);
         if (tabuleiro[index] == 'x') {
             tabuleiro[index] = '.'; 
         } else {
             tabuleiro[index] = 'x'; 
         }
-    }
-    else if (tecla == 'x') {
+    } else if (tecla == 'x') {
         return true; 
     }
 
@@ -116,7 +102,7 @@ bool verificaLinhas(int tamanhoTabuleiro, int numero, char tabuleiro[]) {
     int numeroRainhas;
     for (int i = 0; i < tamanhoTabuleiro; i += numero) {
         numeroRainhas = 0;
-        for (int j = i; j < i + numero; j++) {
+        for (int j = i; j < i + numero && j < tamanhoTabuleiro; j++) {
             if (tabuleiro[j] == 'x') {
                 numeroRainhas++;
             }
@@ -132,7 +118,7 @@ bool verificaColunas(int tamanhoTabuleiro, int numero, char tabuleiro[]) {
     int numeroRainhas;
     for (int coluna = 0; coluna < numero; coluna++) {
         numeroRainhas = 0;
-        for (int j = 0; j < tamanhoTabuleiro; j += numero) {
+        for (int j = coluna; j < tamanhoTabuleiro; j += numero) {
             if (tabuleiro[j] == 'x') {
                 numeroRainhas++;
             }
@@ -146,10 +132,12 @@ bool verificaColunas(int tamanhoTabuleiro, int numero, char tabuleiro[]) {
 
 bool verificaDiagonal(int tamanhoTabuleiro, int numero, char tabuleiro[]) {
     int numeroRainhas;
-    for(int i = 0; i < tamanhoTabuleiro; i++) {
+
+    // Verifica diagonais da esquerda para a direita
+    for (int i = 0; i < tamanhoTabuleiro; i++) {
         numeroRainhas = 0;
-        for(int posicaoInicial = i; posicaoInicial < tamanhoTabuleiro; posicaoInicial += numero + 1) {
-            if(tabuleiro[posicaoInicial] == 'x') {
+        for (int posicaoInicial = i; posicaoInicial < tamanhoTabuleiro; posicaoInicial += numero + 1) {
+            if (posicaoInicial < tamanhoTabuleiro && tabuleiro[posicaoInicial] == 'x') {
                 numeroRainhas++;
             }
         }
@@ -158,44 +146,37 @@ bool verificaDiagonal(int tamanhoTabuleiro, int numero, char tabuleiro[]) {
         }
     }
 
-    for(int j = 0; j < tamanhoTabuleiro; j += numero) {
+    // Verifica diagonais da direita para a esquerda
+    for (int j = 0; j < tamanhoTabuleiro; j++) {
         numeroRainhas = 0;
-        for(int posicaoInicial = j; posicaoInicial < tamanhoTabuleiro; tamanhoTabuleiro += numero - 1) {
-            if (tabuleiro[posicaoInicial] == 'x') {
+        for (int posicaoInicial = j; posicaoInicial < tamanhoTabuleiro; posicaoInicial += numero - 1) {
+            if (posicaoInicial < tamanhoTabuleiro && tabuleiro[posicaoInicial] == 'x') {
                 numeroRainhas++;
             }
-            if (numeroRainhas > 1) {
-                return false;
-            }
         }
-        return true;
+        if (numeroRainhas > 1) {
+            return false;
+        }
     }
+
+    return true; 
 }
 
 bool verificaTabuleiro(int numero, int tamanhoTabuleiro, char tabuleiro[]) {
     return verificaLinhas(tamanhoTabuleiro, numero, tabuleiro) &&
            verificaColunas(tamanhoTabuleiro, numero, tabuleiro) &&
-               verificaDiagonal(tamanhoTabuleiro, numero, tabuleiro);
-}
-
-void recebePosicoes(int tamanhoTabuleiro, char tabuleiro[]) {
-    printf("Insira as posicoes do tabuleiro, sendo que x sao as rainhas e . os espacos vazios: \n");
-    for (int i = 0; i < tamanhoTabuleiro; i++) {
-        char caractere;
-        scanf(" %c", &caractere);
-        while (caractere != 'x' && caractere != '.') {
-            printf("Caractere invalido, insira novamente: \n");
-            scanf(" %c", &caractere);
-        }
-        tabuleiro[i] = caractere;
-    }
-
+           verificaDiagonal(tamanhoTabuleiro, numero, tabuleiro);
 }
 
 int recebeNumeroRainhas() {
-    printf("Insira o número de rainhas:");
     int numeroRainhas;
-    scanf("%d", &numeroRainhas);
+    do {
+        printf("Insira o número de rainhas (maior que 0): ");
+        scanf("%d", &numeroRainhas);
+        if (numeroRainhas <= 0) {
+            printf("Número de rainhas deve ser maior que 0.\n");
+        }
+    } while (numeroRainhas <= 0);
     return numeroRainhas;
 }
 
@@ -204,6 +185,7 @@ void zeraTabuleiro(char tabuleiro[], int tamanho) {
         tabuleiro[i] = '.'; 
     }
 }
+
 int main() {
     t_inicializa();
 
@@ -220,7 +202,7 @@ int main() {
     clock_t inicio = clock();
 
     while (!sair) {
-        desenha_tabuleiro(numeroRainhas, tabuleiro, linha_cursor, coluna_cursor, 0);
+ desenha_tabuleiro(numeroRainhas, tabuleiro, linha_cursor, coluna_cursor, 0);
         sair = processa_entrada(tabuleiro, numeroRainhas, &linha_cursor, &coluna_cursor);
         if (verificaTabuleiro(numeroRainhas, tamanhoTabuleiro, tabuleiro)) {
             venceu = true;
@@ -229,18 +211,14 @@ int main() {
     }
 
     if (venceu) {
-
         clock_t fim = clock();
         double tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
-
         printf("\nParabéns! Você completou o desafio em %.2f segundos.\n", tempo);
     } else {
         printf("\nPrograma encerrado pelo usuário.\n");
     }
 
     getchar();
-
     t_finaliza();
-
     return 0;
 }
