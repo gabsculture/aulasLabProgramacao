ff92df44ed963aba107c7f7045c20352cb9e94a6
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2024-11-20 15:12:38 -0300

    Update t2.c

--- t2.c
+++ t2.c
@@ -1,101 +1,85 @@
-#include <stdio.h>
+//l1-t2 gabriela vitoria da rosa soares
 #include <stdbool.h>
 #include "terminal.h"
 #include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-// Função para desenhar o tabuleiro
-void desenha_tabuleiro(int tamanho, char tabuleiro[], int linha_destaque, int coluna_destaque, int estado) {
-    // Cores de fundo alternadas
-    int cor_fundo_clara[] = {220, 220, 220}; // Cinza claro
-    int cor_fundo_escura[] = {100, 100, 100}; // Cinza escuro
-    int cor_destaque[] = {0, 255, 0};        // Verde (posição em destaque)
+void desenha_tabuleiro(int tamanho, char tabuleiro[], int linha_destaque, int coluna_destaque) {
+    int cor_fundo_clara[] = {220, 220, 220};
+    int cor_fundo_escura[] = {100, 100, 100};
+    int cor_destaque[] = {0, 255, 0};
 
-    // Cores da borda do tabuleiro para representar o estado
-    int cor_borda_ok[] = {0, 255, 0};        // Verde (tabuleiro válido)
-    int cor_borda_alerta[] = {255, 255, 0};  // Amarelo (aviso)
-    int cor_borda_erro[] = {255, 0, 0};      // Vermelho (tabuleiro inválido)
-
-    // Escolhe a cor da borda com base no estado
-    int *cor_borda;
-    if (estado == 1) {
-        cor_borda = cor_borda_ok;
-    } else if (estado == 2) {
-        cor_borda = cor_borda_alerta;
-    } else {
-        cor_borda = cor_borda_erro;
-    }
-
-    // Limpa o terminal antes de desenhar
-    t_limpa();
-
-    // Desenha a borda
-    for (int i = 0; i < tamanho + 2; i++) {
-        for (int j = 0; j < tamanho + 2; j++) {
-            if (i == 0 || i == tamanho + 1 || j == 0 || j == tamanho + 1) {
-                t_cor_fundo(cor_borda[0], cor_borda[1], cor_borda[2]);
-            } else {
-                t_cor_fundo(0, 0, 0); // Fundo preto para área dentro do tabuleiro
-            }
-            t_lincol(i, j * 2); // Espaçamento horizontal ajustado para visualização
-            printf("  ");
-        }
-    }
-
-    // Desenha as células do tabuleiro
     for (int linha = 1; linha <= tamanho; linha++) {
         for (int coluna = 1; coluna <= tamanho; coluna++) {
-            // Determina a cor de fundo
             int *cor_atual;
             if (linha == linha_destaque && coluna == coluna_destaque) {
-                cor_atual = cor_destaque; // Destaque para posição selecionada
+                cor_atual = cor_destaque;
             } else if ((linha + coluna) % 2 == 0) {
                 cor_atual = cor_fundo_clara;
             } else {
                 cor_atual = cor_fundo_escura;
             }
 
-            // Define a cor de fundo
             t_cor_fundo(cor_atual[0], cor_atual[1], cor_atual[2]);
-
-            // Move o cursor e imprime a célula
-            t_lincol(linha, coluna * 2);
             printf(" %c", tabuleiro[(linha - 1) * tamanho + (coluna - 1)]);
         }
+        printf("\n");
     }
-
-    // Restaura a cor normal e atualiza o terminal
     t_cor_normal();
     t_atualiza();
 }
 
-bool processa_entrada(char tabuleiro[], int tamanho, int *linha_cursor, int *coluna_cursor) {
-    if (!t_tem_tecla()) {
-        return false; 
-    }
 
-    char tecla = t_tecla();
+bool processa_entrada(char tabuleiro[], int tamanho, int *linha_cursor, int *coluna_cursor, int *contadorRainhas,
+                      bool *validando) {
+    int nlin, ncol;
+    t_tamanho(&nlin, &ncol);
+    if (t_tem_tecla()) {
+        char tecla = t_tecla();
 
-    ```c
-    if (tecla == 'w' && *linha_cursor > 1) {
-        (*linha_cursor)--;
-    } else if (tecla == 's' && *linha_cursor < tamanho) {
-        (*linha_cursor)++;
-    } else if (tecla == 'a' && *coluna_cursor > 1) {
-        (*coluna_cursor)--;
-    } else if (tecla == 'd' && *coluna_cursor < tamanho) {
-        (*coluna_cursor)++;
-    } else if (tecla == '\n' || tecla == ' ') {
-        int index = ((*linha_cursor - 1) * tamanho) + (*coluna_cursor - 1);
-        if (tabuleiro[index] == 'x') {
-            tabuleiro[index] = '.'; 
-        } else {
-            tabuleiro[index] = 'x'; 
+        switch (tecla) {
+            case 'w':
+                if (*linha_cursor > 1) (*linha_cursor)--;
+                break;
+            case 's':
+                if (*linha_cursor < nlin - 1) (*linha_cursor)++;
+                break;
+            case 'a':
+                if (*coluna_cursor > 1) (*coluna_cursor)--;
+                break;
+            case 'd':
+                if (*coluna_cursor < ncol - 1) (*coluna_cursor)++;
+                break;
+            case ' ':
+            case '\n': {
+                int index = ((*linha_cursor - 1) * tamanho) + (*coluna_cursor - 1);
+                if (tabuleiro[index] == '.') {
+                    tabuleiro[index] = 'x';
+                    t_atualiza();
+                } else if (tabuleiro[index] == 'x') {
+
+                    tabuleiro[index] = '.';
+                    t_atualiza();
+                }
+                *contadorRainhas = 0;
+                for (int i = 0; i < tamanho * tamanho; i++) {
+                    if (tabuleiro[i] == 'x') {
+                        (*contadorRainhas)++;
+                    }
+
+                }
+                if (*contadorRainhas == tamanho) {
+                    *validando = true;
+                }
+                break;
+            case 'x':
+                return true;
+            }
         }
-    } else if (tecla == 'x') {
-        return true; 
-    }
 
-    return false; 
+        return false;
+    }
 }
 
 bool verificaLinhas(int tamanhoTabuleiro, int numero, char tabuleiro[]) {
@@ -133,7 +117,6 @@ bool verificaColunas(int tamanhoTabuleiro, int numero, char tabuleiro[]) {
 bool verificaDiagonal(int tamanhoTabuleiro, int numero, char tabuleiro[]) {
     int numeroRainhas;
 
-    // Verifica diagonais da esquerda para a direita
     for (int i = 0; i < tamanhoTabuleiro; i++) {
         numeroRainhas = 0;
         for (int posicaoInicial = i; posicaoInicial < tamanhoTabuleiro; posicaoInicial += numero + 1) {
@@ -146,7 +129,6 @@ bool verificaDiagonal(int tamanhoTabuleiro, int numero, char tabuleiro[]) {
         }
     }
 
-    // Verifica diagonais da direita para a esquerda
     for (int j = 0; j < tamanhoTabuleiro; j++) {
         numeroRainhas = 0;
         for (int posicaoInicial = j; posicaoInicial < tamanhoTabuleiro; posicaoInicial += numero - 1) {
@@ -159,7 +141,7 @@ bool verificaDiagonal(int tamanhoTabuleiro, int numero, char tabuleiro[]) {
         }
     }
 
-    return true; 
+    return true;
 }
 
 bool verificaTabuleiro(int numero, int tamanhoTabuleiro, char tabuleiro[]) {
@@ -168,57 +150,98 @@ bool verificaTabuleiro(int numero, int tamanhoTabuleiro, char tabuleiro[]) {
            verificaDiagonal(tamanhoTabuleiro, numero, tabuleiro);
 }
 
+bool verificaQuantidadeRainhas(int numero, char tabuleiro[]) {
+    int contadorRainhas = 0;
+    for (int i = 0; i < numero * numero; i++) {
+        if (tabuleiro[i] == 'x') {
+            contadorRainhas++;
+        }
+    }
+    if (contadorRainhas < numero) {
+        return false;
+    }
+    return true;
+}
+
 int recebeNumeroRainhas() {
     int numeroRainhas;
-    do {
-        printf("Insira o número de rainhas (maior que 0): ");
-        scanf("%d", &numeroRainhas);
-        if (numeroRainhas <= 0) {
-            printf("Número de rainhas deve ser maior que 0.\n");
-        }
-    } while (numeroRainhas <= 0);
+    printf("Insira o número de rainhas (maior que 0):\n ");
+    scanf("%d", &numeroRainhas);
     return numeroRainhas;
 }
 
-void zeraTabuleiro(char tabuleiro[], int tamanho) {
-    for (int i = 0; i < tamanho * tamanho; i++) {
-        tabuleiro[i] = '.'; 
+char *inicializaTabuleiro(int tamanhoTabuleiro) {
+    char *tabuleiro = (char *) malloc(tamanhoTabuleiro * sizeof(char));
+
+    if (tabuleiro == NULL) {
+        printf("Erro: Não foi possível alocar memória para o tabuleiro.\n");
+        return NULL;
     }
+
+    for (int i = 0; i < tamanhoTabuleiro; i++) {
+        tabuleiro[i] = '.';
+    }
+
+    return tabuleiro;
 }
 
 int main() {
-    t_inicializa();
+    bool fim = false;
+    int nlin, ncol;
+    int linha_destaque, coluna_destaque;
+    int linha_cursor = 1, coluna_cursor = 1;
+    double ultima_mexida;
+
+
+    linha_destaque = 2 + rand() % (nlin - 4);
+    coluna_destaque = 2 + rand() % (ncol - 4);
+
 
     int numeroRainhas = recebeNumeroRainhas();
     int tamanhoTabuleiro = numeroRainhas * numeroRainhas;
-    char tabuleiro[tamanhoTabuleiro];
-    int linha_cursor = 1, coluna_cursor = 1; 
-    int estado = 1; 
-    bool sair = false;
-    bool venceu = false;
+    char *tabuleiro = inicializaTabuleiro(tamanhoTabuleiro);
+    int contadorRainhas = 0;
+    bool validando = false;
 
-    zeraTabuleiro(tabuleiro, numeroRainhas);
+    t_inicializa();
 
-    clock_t inicio = clock();
+    t_tamanho(&nlin, &ncol);
+    ultima_mexida = t_relogio();
 
-    while (!sair) {
- desenha_tabuleiro(numeroRainhas, tabuleiro, linha_cursor, coluna_cursor, 0);
-        sair = processa_entrada(tabuleiro, numeroRainhas, &linha_cursor, &coluna_cursor);
-        if (verificaTabuleiro(numeroRainhas, tamanhoTabuleiro, tabuleiro)) {
-            venceu = true;
-            sair = true; 
+    if (tabuleiro == NULL) {
+        t_finaliza();
+        return 1;
+    }
+
+    desenha_tabuleiro(numeroRainhas, tabuleiro, linha_cursor, coluna_cursor);
+
+    while (!fim) {
+        fim = processa_entrada(tabuleiro, numeroRainhas, &linha_cursor, &coluna_cursor, &contadorRainhas, &validando);
+
+        if (t_relogio() - ultima_mexida >= 8.2) {
+            linha_destaque = 2 + rand() % (nlin - 4);
+            coluna_destaque = 2 + rand() % (ncol - 4);
+            ultima_mexida = t_relogio(); // Atualiza o tempo da última mudança
+        }
+
+        if (validando) {
+            if (contadorRainhas == numeroRainhas) {
+                if (verificaTabuleiro(numeroRainhas, tamanhoTabuleiro, tabuleiro)) {
+                    printf("\nParabéns! Você completou o desafio!\n");
+                }
+            } else {
+                printf("\nTabuleiro inválido!\n");
+            }
+            break;
         }
     }
 
-    if (venceu) {
-        clock_t fim = clock();
-        double tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
-        printf("\nParabéns! Você completou o desafio em %.2f segundos.\n", tempo);
-    } else {
+    if (!validando) {
         printf("\nPrograma encerrado pelo usuário.\n");
     }
-
-    getchar();
     t_finaliza();
+
+    free(tabuleiro);
+
     return 0;
 }
